faaf4db3bf72f4659ec2f8062fa22245b6f3b902
fbraem <franky.braem@gmail.com>
2021-04-24 18:55:54 +0200

    Set distutils properties

--- build.py
+++ build.py
@@ -29,4 +29,24 @@ def set_properties(project):
 
     project.set_property("flake8_verbose_output", True)
 
-
+    project.set_property("distutils_readme_description", True)
+    project.set_property("distutils_description_overwrite", True)
+    project.set_property("distutils_readme_file", "README.rst")
+    project.set_property("distutils_classifiers", [
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+    ])
+    project.set_property("distutils_setup_keywords", [
+        "sql-smith", "query", "builder", "querybuilder", "sql", "mysql", "sqlite", "postgres", "sqlserver", "database"
+    ])
+    project.set_property("distutils_upload_repository_key", "testpypi")
