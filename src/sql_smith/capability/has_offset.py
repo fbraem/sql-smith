cffe7be4ecbe0aa11dbc334f512b7657a408564d
fbraem <franky.braem@gmail.com>
2023-05-15 17:18:14 +0200

    feat: add has_offset property

--- src/sql_smith/capability/has_offset.py
+++ src/sql_smith/capability/has_offset.py
@@ -6,6 +6,11 @@ class HasOffsetMixin:
         self._offset = new_offset
         return self
 
+    @property
+    def has_offset(self):
+        """Return True when the query has an offset."""
+        return self._offset is not None
+
     def _apply_offset(self, query: "ExpressionInterface") -> "ExpressionInterface":
         return (
             query.append("OFFSET {}", literal(self._offset)) if self._offset else query
