72587f221ff6c36ed876702c0fed9829ad84d7e6
fbraem <franky.braem@gmail.com>
2023-05-15 17:17:57 +0200

    feat: add is_limited

--- src/sql_smith/capability/has_limit.py
+++ src/sql_smith/capability/has_limit.py
@@ -6,5 +6,10 @@ class HasLimitMixin:
         self._limit = new_limit
         return self
 
+    @property
+    def is_limited(self) -> bool:
+        """Returns True when the query has a LIMIT."""
+        return self._limit is not None
+
     def _apply_limit(self, query: "ExpressionInterface") -> "ExpressionInterface":
         return query.append("LIMIT {}", literal(self._limit)) if self._limit else query
